d988db6316c71257afac32fb432461246bb04aa2
jaxon98 <winnj2@ferris.edu>
2020-12-07 14:43:23 -0500

    Added UI
    added functional ui to the game

---
+++ ProjectIntrovert/Assets/Scripts/ScoringSystem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoringSystem : MonoBehaviour
+{
+    public GameObject scoreText;
+    public int theScore;
+    public AudioSource collectSound;
+
+    void OnTriggerEnter(Collider other) 
+    {
+        collectSound.Play();
+        Destroy(gameObject);
+    
+    }
+}
